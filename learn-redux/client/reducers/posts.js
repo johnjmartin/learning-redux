7f6affa43c00b06594c9bb5afbedf51c155c8558
John Martin <johnmart@ciena.com>
2016-08-29 10:34:38 -0400

    12th video

---
+++ learn-redux/client/reducers/posts.js
@@ -0,0 +1,23 @@
+// a reducer takes in two things:
+
+// 1. the action (info about what happened)
+
+// 2. copy of current state
+
+function posts(state = [], action) {
+	switch(action.type) {
+		case 'INCREMENT_LIKES':
+			console.log("INCREMENT_LIKEs")
+			return [
+				...state.slice(0,i),
+				{...state[i], likes: state[i].likes + 1},
+				...state.slice(i + 1),
+			]
+		default:
+			return state;
+	}
+	console.log(state, action);
+	return state;
+}
+
+export default posts;
